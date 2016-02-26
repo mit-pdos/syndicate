42c459b1edb446bab75fb125b3c2259c2a8ff97b
Jon Gjengset <jon@thesquareplanet.com>
2016-02-26 17:05:33 -0500

    Client will now also check for new workers

--- test-src/src/6824/gfs/client/main.go
+++ test-src/src/6824/gfs/client/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"k8s.io/kubernetes/pkg/api"
 	client "k8s.io/kubernetes/pkg/client/unversioned"
@@ -54,6 +55,15 @@ func (c *Client) Pods(component string) []string {
 	return wids
 }
 
+func (c *Client) State(podName string) (string, error) {
+	p, err := c.kubernetes.Pods(api.NamespaceDefault).Get(podName)
+	if err != nil {
+		return "", err
+	}
+
+	return string(p.Status.Phase), nil
+}
+
 func (c *Client) Kill(podName string) error {
 	return c.kubernetes.Pods(api.NamespaceDefault).Delete(podName, &api.DeleteOptions{})
 }
@@ -88,19 +98,96 @@ func main() {
 
 	c := New(masterAddress)
 
-	var chunkservers []string
-	err := c.MRPC("Servers", &struct{}{}, &chunkservers)
-	if err != nil {
-		log.Fatal("get chunkservers:", err)
+	var wids []string
+	fmt.Println("Waiting for all workers to be scheduled")
+	for len(wids) != workers {
+		wids = c.Pods(fmt.Sprintf("%s-worker", user))
+		<-time.After(50 * time.Millisecond)
+	}
+	for _, wid := range wids {
+		fmt.Println(" -", wid)
 	}
 
-	wids := c.Pods(fmt.Sprintf("%s-worker", user))
-	var wips []string
+	fmt.Println("Waiting for all workers to be running")
+	for _, wid := range wids {
+		var err error
+		var p string
+		for {
+			p, err = c.State(wid)
+			if err != nil {
+				log.Fatal("unknown worker", wid, err)
+			}
+			if p == "Running" {
+				break
+			}
+			if p != "Pending" {
+				log.Fatal("worker in unknown state", p)
+			}
+		}
+	}
+
+	var chunkservers []string
+	fmt.Println("Waiting for all workers to register with master")
+	for len(chunkservers) != workers {
+		chunkservers = chunkservers[:0]
+		err := c.MRPC("Servers", &struct{}{}, &chunkservers)
+		if err != nil {
+			log.Fatal("get chunkservers:", err)
+		}
+		<-time.After(50 * time.Millisecond)
+	}
+	for _, cs := range chunkservers {
+		fmt.Println(" -", cs)
+	}
+
+	fmt.Println("Getting pod IPs")
+	wips := make([]string, 0, len(wids))
 	for _, wid := range wids {
 		wips = append(wips, c.PodIP(wid))
 	}
+	for _, wip := range wips {
+		fmt.Println(" -", wip)
+	}
 
 	fmt.Println("chunkservers:", chunkservers, "\nwidp:", wips)
+
 	fmt.Printf("about to kill %s\n", c.PodIP(wids[0]))
 	c.Kill(wids[0])
+
+	i := 0
+	for i < 10 {
+		p, err := c.State(wids[0])
+		if err != nil {
+			fmt.Println("getting pod state returned:", err)
+			break
+		}
+		if p == "Running" {
+			continue
+		}
+		if p == "Terminating" {
+			continue
+		}
+		fmt.Println("pod state is:", p)
+		<-time.After(50 * time.Millisecond)
+		i++
+	}
+
+	i = 0
+	var nchunkservers []string
+	fmt.Println("checking that master eventually gets another chunkserver")
+	for len(nchunkservers) <= len(chunkservers) && i < 10 {
+		nchunkservers = nchunkservers[:0]
+		err := c.MRPC("Servers", &struct{}{}, &nchunkservers)
+		if err != nil {
+			log.Fatal("get chunkservers:", err)
+		}
+		<-time.After(200 * time.Millisecond)
+		i++
+	}
+
+	if len(nchunkservers) > len(chunkservers) {
+		fmt.Println("master got new chunkserver", nchunkservers[len(nchunkservers)-1])
+	} else {
+		fmt.Println("ERROR: master did not get a new chunkserver. compare", chunkservers, nchunkservers)
+	}
 }
